97af3a129f88244fec87256b2804df8abd879c4e
isahua16 <isaelhuard@gmail.com>
2023-04-06 22:12:51 -0600

    added the get tweets functionality and more stylings

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+// ------------------------------------ Post function ----------------------------------------------
+
 function post_tweet(event)
 {   
     let tweet_body = document.querySelector(`#text_box`)[`value`];
     if(tweet_body === ``)
     {
-        document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#fa5252;" id="user_msg">Write something to post!</p>`);
-        setTimeout(display_notification, 10);
-        setTimeout(remove_notification, 3000);
+        display_error_message(`Write something to post!`);
         return;
     }
     else
@@ -24,14 +24,11 @@ function post_tweet(event)
             }
         ).then(tweet_success).catch(tweet_error);
     }
-
 }
 
 function tweet_success(res)
 {
-    document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#339af0; "id="user_msg">Your tweet was posted!</p>`);
-    setTimeout(display_notification, 10);
-    setTimeout(remove_notification, 3000);
+    display_success_message(`Your tweet was posted`)
     document.querySelector(`#text_box`)[`value`] = ``;
 }
 
@@ -47,10 +44,50 @@ function remove_notification(event)
 
 function tweet_error(err)
 {
-    document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#fa5252;" id="user_msg">Something went wrong. Try again!</p>`);
+    display_error_message(`Something went wrong. Try again!`);
+}
+
+function display_success_message(message)
+{
+    document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#339af0; "id="user_msg">${message}</p>`);
+    setTimeout(display_notification, 10);
+    setTimeout(remove_notification, 3000);
+}
+
+function display_error_message(message)
+{
+    document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#fa5252;" id="user_msg">${message}</p>`);
     setTimeout(display_notification, 10);
     setTimeout(remove_notification, 3000);
 }
 
 let tweet_button = document.querySelector(`#submit_button`);
 tweet_button.addEventListener(`click`, post_tweet);
+
+// ------------------------------------ Get function ----------------------------------------------
+
+function get_tweets_success(res)
+{
+    for(let i = 0; i < res[`data`].length; i++)
+    {
+        tweet_container.insertAdjacentHTML(`beforeend`, 
+        `<article class=tweet_card>
+            <h4>@${res[`data`][i][`userId`]}</h4>
+            <p>${res[`data`][i][`body`]}</p>
+        </article>`
+        );
+    }
+}
+
+function get_tweets_error(err)
+{
+    display_error_message(`Something is wrong. Refresh the page`)
+}
+
+let tweet_container = document.querySelector(`#tweet_container`);
+
+axios.request(
+    {
+        url: `https://jsonplaceholder.typicode.com/posts`,
+    }
+).then(get_tweets_success).catch(get_tweets_error);
