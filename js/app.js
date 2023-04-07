dbea52f46b0a96bd4ac3051e0db738cb6968ebec
isahua16 <isaelhuard@gmail.com>
2023-04-06 22:27:54 -0600

    Add edit and delete functionalities

--- js/app.js
+++ js/app.js
@@ -49,6 +49,11 @@ function tweet_error(err)
 
 function display_success_message(message)
 {
+    if(document.querySelector(`#user_msg`))
+    {
+        document.querySelector(`#user_msg`).remove();
+    }
+    
     document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#339af0; "id="user_msg">${message}</p>`);
     setTimeout(display_notification, 10);
     setTimeout(remove_notification, 3000);
@@ -56,6 +61,10 @@ function display_success_message(message)
 
 function display_error_message(message)
 {
+    if(document.querySelector(`#user_msg`))
+    {
+        document.querySelector(`#user_msg`).remove();
+    }
     document.querySelector(`body`).insertAdjacentHTML(`afterend`, `<p style="opacity: 0; background-color:#fa5252;" id="user_msg">${message}</p>`);
     setTimeout(display_notification, 10);
     setTimeout(remove_notification, 3000);
@@ -91,3 +100,57 @@ axios.request(
         url: `https://jsonplaceholder.typicode.com/posts`,
     }
 ).then(get_tweets_success).catch(get_tweets_error);
+
+// ------------------------------------ Patch function ----------------------------------------------
+
+function edit_tweet(event)
+{
+    axios.request(
+        {
+            url: `https://jsonplaceholder.typicode.com/posts/1`,
+            method: `PATCH`,
+            data: 
+                {
+                    body: `Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists. Yes, Yes, without the oops! Checkmate... Life finds a way. Did he just throw my cat out of the window? Hey, take a look at the earthlings. Goodbye! You really think you can fly that thing?`,
+                }
+        }
+    ).then(edit_tweet_success).catch(edit_tweet_error);
+}
+
+function edit_tweet_success(res)
+{
+    display_success_message(`Updated tweet succesfully`);
+}
+
+function edit_tweet_error(err)
+{
+    display_error_message(`Failed to edit the tweet`);
+}
+
+let edit_button = document.querySelector(`#edit_button`);
+edit_button.addEventListener(`click`, edit_tweet);
+
+// ------------------------------------ Delete function ----------------------------------------------
+
+function delete_tweet(event)
+{
+    axios.request(
+        {
+            url: `https://jsonplaceholder.typicode.com/posts/1`,
+            method: `DELETE`,
+        }
+    ).then(delete_tweet_success).catch(delete_tweet_error);
+}
+
+function delete_tweet_success(res)
+{
+    display_success_message(`Deleted tweet succesfully`);
+}
+
+function delete_tweet_error(err)
+{
+    display_error_message(`Failed to delete the tweet`);
+}
+
+let delete_button = document.querySelector(`#delete_button`);
+delete_button.addEventListener(`click`, delete_tweet);
